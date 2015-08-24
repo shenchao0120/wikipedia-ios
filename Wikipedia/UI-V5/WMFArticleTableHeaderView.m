f94e9711a33cbf240a93c8c07e492a7d030be971
Corey Floyd <coreyfloyd@gmail.com>
2015-08-24 17:42:25 -0400

    Adding bookmark icon

--- Wikipedia/UI-V5/WMFArticleTableHeaderView.m
+++ Wikipedia/UI-V5/WMFArticleTableHeaderView.m
@@ -18,9 +18,8 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     self.initialDescriptionTopConstraintConstant = self.descriptionTopConstraint.constant;
-    [self.saveButton wmf_setButtonType:WMFButtonTypeHeart];
-    self.titleLabel.layer.shadowRadius       = 5.0;
-    self.descriptionLabel.layer.shadowRadius = 5.0;
+    self.titleLabel.layer.shadowRadius           = 5.0;
+    self.descriptionLabel.layer.shadowRadius     = 5.0;
 }
 
 - (void)setTitle:(NSString*)title description:(NSString*)description {
